d30cd72633657fdeda994c50a914973833cfe745
Prajith-123 <kumarprajith944@gmail.com>
2025-07-03 11:55:43 +0530

    Fix: final vercel build setup

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
-pip install -r requirements.txt
-apt-get install build-essentials
+#!/bin/bash
+
+# Fix missing system packages for pip builds
+apt-get update
+apt-get install -y python3-distutils
+
+# Install Python packages locally for Vercel
+pip install --upgrade -r requirements.txt --target .
